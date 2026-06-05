4befa60bced7dc14e635f6630e3d692c84d41314
Julian Federico Viel <jfviel@hotmail.com>
2026-06-04 23:32:34 -0300

    Testing PickUp Sword

--- Assets/Script/Levels/Common/RoomController.cs
+++ Assets/Script/Levels/Common/RoomController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class RoomController : MonoBehaviour
 {
     [SerializeField] public Transform playerEnterPosition;
+    [SerializeField] RewardUI rewardUI;
     public Transform GetEnterPosition() => playerEnterPosition;
     List<Enemy> enemies = new List<Enemy>();
     List<EnemySpawner> spawners = new List<EnemySpawner>();
@@ -21,7 +22,7 @@ public class RoomController : MonoBehaviour
     {
         enemies.Remove(enemy);
         if (enemies.Count == 0)
-            Debug.Log("roomCleared");
+            rewardUI.Show();
     }
     public void EnemySpawned(Enemy enemy)
     {
