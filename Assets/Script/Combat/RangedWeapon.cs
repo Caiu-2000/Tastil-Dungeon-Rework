c86307ab9a295c184670b67c3f5578142c3b0746
Caiu-2000 <totaroezequiel2004@gmail.com>
2026-08-05 03:01:19 -0300

    Special Attack Working (Now we need attacks but they are called xD)

--- Assets/Script/Combat/RangedWeapon.cs
+++ Assets/Script/Combat/RangedWeapon.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 public class RangedWeapon : Weapon
 {
     [SerializeField] Proyectile _arrow;
+    [SerializeField] Proyectile _SpecialArrow;
     [SerializeField] Transform _firePoint;
     private Animator animator;
     private bool _IsCharging = false;
 
-
+    private float currentTension = 0f;
+    [SerializeField] private float MaxTension = 3.0f;
 
 
     private void Start()
@@ -17,6 +19,19 @@ public class RangedWeapon : Weapon
         _firstPosition = transform.position;
         _ItemCollider = GetComponent<Collider>();
 
+    }
+
+    private void Update()
+    {
+        if (_IsCharging)
+        {
+            currentTension += Time.deltaTime;
+            if (currentTension > MaxTension)
+            {
+                currentTension = MaxTension;
+
+            }
+        }
     }
 
     public override void ChargeAttack()
@@ -30,17 +45,40 @@ public class RangedWeapon : Weapon
         
         _IsCharging=false;
         animator.SetTrigger("Release");
-        Proyectile arrowInstance = Instantiate(_arrow);
+
+        ShotArrow(_arrow);
+    }
+    public override void ChargeSpecial()
+    {
+        base.ChargeSpecial();
+        animator.SetTrigger("StartCharge");
+        _IsCharging = true;
+    }
+    public override void ReleaseSpecial()
+    {
+        base.ReleaseSpecial();
+        if (!_IsCharging) return;
+
+        _IsCharging = false;
+        animator.SetTrigger("Release");
+
+        ShotArrow(_SpecialArrow);
+    }
+    private void ShotArrow(Proyectile arrow)
+    {
+        Proyectile arrowInstance = Instantiate(arrow);
         Vector3 FiringOffset = new Vector3(0, 0.5f, 0);
         arrowInstance.transform.position = GameManager.Instance.Player.transform.position + FiringOffset;
-        arrowInstance._speed *= 3;
+        arrowInstance._speed *= 3 * currentTension;
 
         Vector3 AimedPos = GameManager.Instance.Player.GetLookDretirection();
 
-        
+
         arrowInstance.ChangeDirection(AimedPos);
         arrowInstance._fromPlayer = true;
         arrowInstance._damage = _damage;
+
+        currentTension = 0;
     }
 
 }
