3fcf0c41536fb4d48c95a28022a4272e7cbbada0
Caiu-2000 <totaroezequiel2004@gmail.com>
2026-08-05 22:57:46 -0300

    OrderAndFirstTryInAnimations

--- Assets/Script/Player/WeaponsHand.cs
+++ Assets/Script/Player/WeaponsHand.cs
@@ -11,8 +11,7 @@ public class WeaponsHand : MonoBehaviour
 
     void Start()
     {
-        WeaponAnimator = GetComponent<Animator>();
-
+   
         WeaponAnimator.runtimeAnimatorController = overrideController;
         print(WeaponAnimator.runtimeAnimatorController.name);
     }
